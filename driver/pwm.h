0b1e6977f63fac63a4ea00a406c2388152f42450
110101001 <1097727091@qq.com>
2017-08-10 01:34:40 +0800

    调通PWM输出功能
    将PWM输出定时器由TIM3改为TIM4，因TIM3已用作定时控制函数的定时器

--- driver/pwm.h
+++ driver/pwm.h
@@ -3,17 +3,17 @@
 
 #define DUTY_MAX 999
 
-#define DUTY1 (TIM3->CCR1)
-#define DUTY2 (TIM3->CCR2)
-#define DUTY3 (TIM3->CCR3)
-#define DUTY4 (TIM3->CCR4)
+#define DUTY1 (TIM4->CCR1)
+#define DUTY2 (TIM4->CCR2)
+#define DUTY3 (TIM4->CCR3)
+#define DUTY4 (TIM4->CCR4)
 
 void PWM_Configuration(void);
 
 #define SetPWMOut(CH1,CH2,CH3,CH4) \
-											TIM3->CCR1=CH1;\
-											TIM3->CCR2=CH2;\
-											TIM3->CCR3=CH3;\
-											TIM3->CCR4=CH4;
+											TIM4->CCR1=CH1;\
+											TIM4->CCR2=CH2;\
+											TIM4->CCR3=CH3;\
+											TIM4->CCR4=CH4;
 
 #endif
